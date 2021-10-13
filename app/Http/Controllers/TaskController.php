a8714b6bfac5801117cbe026f375319154781797
shinjyou <yuuya.shinjou@gmail.com>
2021-10-14 07:26:26 +0900

    todo 1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Folder;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(int $id)
     {
-        return "hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+        ]);
     }
 }
